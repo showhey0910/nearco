3491b15a1cbcd457445fd0f0a83d73eeef5071c8
showhey0910 <53123188+showhey0910@users.noreply.github.com>
2020-08-10 14:54:13 +0900

    Modify prism.js & prism.css | Languages(ERB, PHP, Python, Ruby, Scss), Plugins(Line Highlight, Line Numbers, Autolinker, Show Language, Toolbar, Copy to Clipboard Button)

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css">
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/prism-filename.css">
   <?php wp_head(); ?>
 </head>
 
