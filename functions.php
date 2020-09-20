d03d28711e1d2ea95c1c316e066cf84a68ce2771
showhey0910 <53123188+showhey0910@users.noreply.github.com>
2020-09-20 13:34:28 +0900

    Add title-tag

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+add_theme_support( 'title-tag' );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( 'post-thumbnails' );
 register_nav_menu( 'header-nav', ' ヘッダーナビゲーション ' );
